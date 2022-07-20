0fdac6986a08b130eb892f60105b982ac2a6fa6c
Krisna Sirait <krisnapande00@outlook.com>
2022-07-21 00:21:20 +0700

    added code for check result and get bot inpuyt inside MainActivity.kt

--- app/src/main/java/com/krisna/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/krisna/rockpaperscissors/MainActivity.kt
@@ -1,12 +1,59 @@
 package com.krisna.rockpaperscissors
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.krisna.rockpaperscissors.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var layout: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        layout = ActivityMainBinding.inflate(layoutInflater)
+        val view = layout.root
+        setContentView(view)
+
+        layout.btnBatu.setOnClickListener {
+            layout.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+        }
+
+        layout.btnKertas.setOnClickListener {
+            layout.btnKertas.setBackgroundColor(ContextCompat.getColor(this, R.color.blue_grey))
+        }
+
+        layout.btnGunting.setOnClickListener {
+            layout.btnGunting.setBackgroundColor(ContextCompat.getColor(this, R.color.blue_grey))
+        }
+    }
+
+    private fun getBotInput(): String{
+        val options = arrayOf("Rock", "Paper", "Scissors")
+        val botOptions: Int = Random.nextInt(options.size)
+        return options[botOptions]
+    }
+
+    private fun checkResult(playerInput: String, comInput: String) {
+        if (playerInput.equals(comInput, true)) {
+            layout.textWin.text = "DRAW"
+            layout.textMenang.text = ""
+            layout.layoutWinner.setBackgroundColor(ContextCompat.getColor(this, R.color.blue))
+        } else if (playerInput.equals("batu", true) && comInput.equals("gunting", true)
+            || playerInput.equals("kertas", true) && comInput.equals("batu", true)
+            || playerInput.equals("gunting", true) && comInput.equals("kertas", true)
+        ) {
+            layout.textWin.text = "Pemain 1"
+            layout.textMenang.text = "MENANG!"
+            layout.layoutWinner.setBackgroundColor(ContextCompat.getColor(this, R.color.light_green))
+        } else if (comInput.equals("batu", true) && playerInput.equals("gunting", true)
+            || comInput.equals("kertas", true) && playerInput.equals("batu", true)
+            || comInput.equals("gunting", true) && playerInput.equals("kertas", true)
+        ) {
+            layout.textWin.text = "Pemain 2"
+            layout.textMenang.text = "MENANG!"
+            layout.layoutWinner.setBackgroundColor(ContextCompat.getColor(this, R.color.light_green))
+        }
     }
 }
