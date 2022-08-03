359025a45d7252f12527e0bfdfe92d64201a232e
Krisna Sirait <krisnapande00@outlook.com>
2022-08-03 12:37:38 +0700

    finished on activity_splash.xml layout

--- app/src/main/java/com/krisna/rockpaperscissors/SplashActivity.kt
+++ app/src/main/java/com/krisna/rockpaperscissors/SplashActivity.kt
@@ -2,10 +2,19 @@ package com.krisna.rockpaperscissors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.load
+import com.krisna.rockpaperscissors.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.imageLogotext.load("https://i.ibb.co/HC5ZPgD/splash-screen1.png")
     }
 }
