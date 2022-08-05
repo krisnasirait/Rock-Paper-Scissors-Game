85137519d823da771b9fa7221146ed21c48752cf
Krisna Sirait <krisnapande00@outlook.com>
2022-08-05 19:07:37 +0700

    created first, second, and third landing page fragments and also added function newInstance inside firs, second, and third kt file

---
+++ app/src/main/java/com/krisna/rockpaperscissors/fragments/SecondLandingPageFragment.kt
@@ -0,0 +1,22 @@
+package com.krisna.rockpaperscissors.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.krisna.rockpaperscissors.R
+
+class SecondLandingPageFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second_landing_page, container, false)
+    }
+
+    companion object {
+        fun newInstance = SecondLandingPageFragment()
+    }
+}
