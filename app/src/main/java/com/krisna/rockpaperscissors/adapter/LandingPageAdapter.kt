25b445299af141c5ffeb90d804f21c76a7776f45
Krisna Sirait <krisnapande00@outlook.com>
2022-08-09 18:23:16 +0700

    added LandingPageAdapter.kt

---
+++ app/src/main/java/com/krisna/rockpaperscissors/adapter/LandingPageAdapter.kt
@@ -0,0 +1,14 @@
+package com.krisna.rockpaperscissors.adapter
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class LandingPageAdapter (activity: AppCompatActivity, private val items: List<Fragment>) : FragmentStateAdapter(activity){
+
+    override fun getItemCount(): Int = items.size
+
+    override fun createFragment(position: Int): Fragment {
+        return items[position]
+    }
+}
