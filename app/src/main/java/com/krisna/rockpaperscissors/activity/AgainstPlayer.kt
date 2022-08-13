f981a32facb3b62730a93cd3b3f3740dba9d7d82
Krisna Sirait <krisnapande00@outlook.com>
2022-08-13 21:26:45 +0700

    added code for AgainstPlayer.kt working

--- app/src/main/java/com/krisna/rockpaperscissors/activity/AgainstPlayer.kt
+++ app/src/main/java/com/krisna/rockpaperscissors/activity/AgainstPlayer.kt
@@ -28,30 +28,26 @@ class AgainstPlayer : AppCompatActivity() {
 
         binding.textPemainOne.text = name
 
-        binding.btnBatu.setOnClickListener {
-            Log.d("User Input", "Batu")
-            playerInput("", "Batu")
-        }
-
-        binding.btnKertas.setOnClickListener {
-            Log.d("User Input", "Kertas")
-            playerInput("", "Kertas")
-        }
-
-        binding.btnGunting.setOnClickListener {
-            Log.d("User Input", "Gunting")
-            playerInput("", "Gunting")
-        }
+        checkResult(playerOneListener(), playerTwoListener())
 
         binding.btnRefresh.setOnClickListener {
             Log.d("User Input", "Button Refresh di Click")
-            playerInput("VS", "Reset")
+
+            binding.btnBatu.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            binding.btnKertas.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            binding.btnGunting.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+
+            binding.btnBatuSecond.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            binding.btnBatuSecond.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
+            binding.btnBatuSecond.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
         }
 
         binding.btnCancel.setOnClickListener {
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 
     private fun btnValue(hasTo: Boolean) {
@@ -60,85 +56,78 @@ class AgainstPlayer : AppCompatActivity() {
         binding.btnBatu.isEnabled = hasTo
     }
 
-    private fun playerInput(textVersus: String, btnPlayer: String) {
+    private fun playerOneListener() : String {
+        lateinit var pOneInput: String
 
-        when (btnPlayer) {
-            "Batu" -> {
-                binding.textVersus.text = textVersus
-                binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
-                checkResult("batu", getBotInput())
-                btnValue(false)
-            }
-            "Kertas" -> {
-                binding.textVersus.text = textVersus
-                binding.btnKertas.setBackgroundResource(R.drawable.roundcorner)
-                checkResult("kertas", getBotInput())
-                btnValue(false)
-
-            }
-            "Gunting" -> {
-                binding.textVersus.text = textVersus
-                binding.btnGunting.setBackgroundResource(R.drawable.roundcorner)
-                checkResult("gunting", getBotInput())
-                btnValue(false)
-            }
-            "Reset" -> {
-                btnValue(true)
-                binding.textVersus.text = textVersus
-
-                binding.btnBatu.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-                binding.btnKertas.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-                binding.btnGunting.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-
-                binding.btnBatuCom.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-                binding.btnKertasCom.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-                binding.btnGuntingCom.setBackgroundColor(ContextCompat.getColor(this, R.color.white))
-            }
+        binding.btnBatu.setOnClickListener {
+            Log.d("User 1 Input", "Batu")
+            binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+            btnValue(false)
+            pOneInput = "Batu"
         }
 
-    }
-
-    private fun getBotInput(): String {
-        val options = arrayOf("batu", "kertas", "gunting")
-        val botOptions: Int = Random.nextInt(options.size)
-        when {
-            options[botOptions] == "batu" -> {
-                Log.d("Bot Input", "Batu")
-                binding.btnBatuCom.setBackgroundResource(R.drawable.roundcorner)
-                toastBot("Batu")
-            }
-            options[botOptions] == "kertas" -> {
-                Log.d("Bot Input", "Kertas")
-                binding.btnKertasCom.setBackgroundResource(R.drawable.roundcorner)
-                toastBot("Kertas")
-            }
-            options[botOptions] == "gunting" -> {
-                Log.d("Bot Input", "Gunting")
-                binding.btnGuntingCom.setBackgroundResource(R.drawable.roundcorner)
-                toastBot("Gunting")
-            }
+        binding.btnKertas.setOnClickListener {
+            Log.d("User 1 Input", "Kertas")
+            binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+            btnValue(false)
+            pOneInput = "Kertas"
         }
-        return options[botOptions]
+
+        binding.btnGunting.setOnClickListener {
+            Log.d("User 1 Input", "Gunting")
+            binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+            btnValue(false)
+            pOneInput = "Gunting"
+        }
+
+        return pOneInput
     }
 
-    private fun checkResult(playerInput: String, comInput: String) {
-        if (playerInput.equals(comInput, true)) {
+    private fun playerTwoListener() : String {
+        lateinit var pTwoInput: String
+
+        binding.btnBatuSecond.setOnClickListener {
+            Log.d("User 1 Input", "Batu")
+            binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+            btnValue(false)
+            pTwoInput = "Batu"
+        }
+
+        binding.btnKertasSecond.setOnClickListener {
+            Log.d("User 1 Input", "Kertas")
+            binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+            btnValue(false)
+            pTwoInput = "Kertas"
+        }
+
+        binding.btnGuntingSecond.setOnClickListener {
+            Log.d("User 1 Input", "Gunting")
+            binding.btnBatu.setBackgroundResource(R.drawable.roundcorner)
+            btnValue(false)
+            pTwoInput = "Gunting"
+        }
+
+        return pTwoInput
+    }
+
+    private fun checkResult(playerInput: String, secondPlayerInput: String) {
+        if (playerInput.equals(secondPlayerInput, true)) {
             Log.d("Hasil", "DRAW")
             val winnerDialogFragment = WinnerDialogFragment("SERI")
             winnerDialogFragment.show(supportFragmentManager, null)
-        } else if (playerInput.equals("batu", true) && comInput.equals("gunting", true)
-            || playerInput.equals("kertas", true) && comInput.equals("batu", true)
-            || playerInput.equals("gunting", true) && comInput.equals("kertas", true)
+        } else if (playerInput.equals("batu", true) && secondPlayerInput.equals("gunting", true)
+            || playerInput.equals("kertas", true) && secondPlayerInput.equals("batu", true)
+            || playerInput.equals("gunting", true) && secondPlayerInput.equals("kertas", true)
         ) {
             Log.d("Hasil", "Pemain 1 Menang")
             val winnerDialogFragment = WinnerDialogFragment(name)
             winnerDialogFragment.show(supportFragmentManager, null)
-        } else if (comInput.equals("batu", true) && playerInput.equals("gunting", true)
-            || comInput.equals("kertas", true) && playerInput.equals("batu", true)
-            || comInput.equals("gunting", true) && playerInput.equals("kertas", true)
+        } else if (secondPlayerInput.equals("batu", true) && playerInput.equals("gunting", true)
+            || secondPlayerInput.equals("kertas", true) && playerInput.equals("batu", true)
+            || secondPlayerInput.equals("gunting", true) && playerInput.equals("kertas", true)
         ) {
             Log.d("Hasil", "Pemain 2 Menang")
-            val winnerDialogFragment = WinnerDialogFragment("CPU")
+            val winnerDialogFragment = WinnerDialogFragment("Pemain 2")
             winnerDialogFragment.show(supportFragmentManager, null)
         }
     }
